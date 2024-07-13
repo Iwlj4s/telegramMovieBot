dcb1ee0edac894986858c8376edaa5c611866655
Iwlj4s <elizavetalenn@mail.ru>
2024-07-13 14:00:15 +0300

    User can add several genres

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -5,8 +5,60 @@ main_keyboard = ReplyKeyboardMarkup(
         [
             KeyboardButton(text="Случайный Фильм"),
             KeyboardButton(text="Случайный Фильм По Жанру"),
+            KeyboardButton(text="Случайный Фильм По Жанру с доп.опциями"),
             KeyboardButton(text="Жанры")
         ],
     ],
     resize_keyboard=True
 )
+
+
+genres_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Аниме"),
+            KeyboardButton(text="Биография"),
+            KeyboardButton(text="Боевик"),
+            KeyboardButton(text="Вестерн"),
+        ],
+
+        [
+            KeyboardButton(text="Военный"),
+            KeyboardButton(text="Детектив"),
+            KeyboardButton(text="Детский"),
+            KeyboardButton(text="Документальный"),
+        ],
+
+        [
+            KeyboardButton(text="Драма"),
+            KeyboardButton(text="Мультфильм"),
+            KeyboardButton(text="Мюзикл"),
+            KeyboardButton(text="Приключения"),
+        ],
+
+        [
+            KeyboardButton(text="Семья"),
+            KeyboardButton(text="Спорт"),
+            KeyboardButton(text="Триллер"),
+            KeyboardButton(text="Ужасы"),
+        ],
+
+        [
+            KeyboardButton(text="Фантастика"),
+            KeyboardButton(text="Фэнтези"),
+        ]
+    ],
+    resize_keyboard=True
+)
+
+yes_no_cancel_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Да"),
+            KeyboardButton(text="Нет"),
+            KeyboardButton(text="Отмена"),
+        ],
+    ],
+    resize_keyboard=True
+)
+
